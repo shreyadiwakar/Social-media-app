62812e685c77a0030d2f8b02c3b7986fb5c93623
shreyadiwakar <diwakar.shreya.2006@gmail.com>
2026-07-12 16:40:20 +0530

    Update connect.js

--- api/connect.js
+++ api/connect.js
@@ -3,4 +3,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const db = mysql.createConnection(process.env.MYSQL_URL);
+export const db = mysql.createConnection({
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+});
